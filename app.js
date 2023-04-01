09567de930411cf4d5ce6a21ef545b2e167b9a6d
Dukepq <maikelvdj@live.be>
2023-04-01 15:54:04 +0200

    /

--- app.js
+++ app.js
@@ -1,67 +1,110 @@
-import { splitWrap } from "./SplitAndWrap.js";
+import {splitWrap} from "./SplitAndWrap.js"
 
-const title = document.querySelector('.title')
-
-
+// setting up the distance function
 function distance(x1, x2, y1, y2) {
     const dx = x2 - x1;
     const dy = y2 - y1;
-    const d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
-    return d
+    return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
 }
 
-// throttle function that will always return the last known position.
-function throttle(callback, timeout = 50) {
-    let wait = false
-    let waitingArgs
-    const timeoutFunc = () => {
-        if (waitingArgs == null) {
-            wait = false
+// splitting and wrapping html text into span
+const title = document.querySelector('.title')
+// const spans = splitWrap(title)
+
+// building mouse object, initiating to (0, 0) and updating the mouse position from there
+const mouse = {
+    x: 0,
+    y: 0,
+}
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+})
+
+// building a virtual position that lags behind the actual mouse position
+const virtualPos = {
+    x: window.innerWidth,
+    y: window.innerHeight,
+}
+
+let maxDistance = title.getBoundingClientRect().width / 2
+
+window.addEventListener('resize', () => {
+    maxDistance = title.getBoundingClientRect().width / 2
+})
+
+
+function Character(parent, character, reverse = false) {
+    let span = document.createElement('span')
+    span.textContent = character
+    parent.appendChild(span)
+    console.log(reverse)
+    this.fetchDistance = () => {
+        this.dimensions = span.getBoundingClientRect()
+        return distance(this.dimensions.x, virtualPos.x, this.dimensions.y, virtualPos.y)
+    }
+    this.updateProperties = (args) => {
+        // console.log(args)
+        const d = this.fetchDistance()
+        this.width = args.weight ? ~~this.CalculateProperties(125, 75, d) : 100
+        this.weight = args.weight ? ~~this.CalculateProperties(900, 100, d) : 300
+        this.ital = this.CalculateProperties(1, 0, d).toFixed(2)
+        this.setProperties()
+    }
+
+    this.CalculateProperties = (maxVal, minVal, distance) => {
+        if (!reverse) {
+            let attr = maxVal - Math.abs(maxVal * distance / maxDistance)
+            return Math.max(minVal, attr + minVal)
         } else {
-            callback(...waitingArgs)
-            waitingArgs = null
-            setTimeout(timeoutFunc, timeout)
+            let attr = (maxVal - (maxVal - Math.abs(maxVal * distance / maxDistance))) * 0.5
+            return attr
+            
         }
     }
-    return (...args) => {
-        if (wait) {
-            waitingArgs = args
-            return
-        }
-        callback(...args)
-        wait = true;
-        setTimeout(timeoutFunc, timeout)
+
+    this.setProperties = () => {
+        span.style.fontStretch = `${this.width}%`
+        span.style.fontWeight = `${this.weight}`
+        span.style.fontVariationSettings = `'ital' ${this.ital}`
     }
+
+    return this
 }
 
 
-splitWrap(title) // stays outside of the function considering it only needs to be called once.
-const animateTextOnMove = throttle((event, spanParent) => {
-    const spanArray = Array.prototype.slice.call(spanParent.children) //element.children returns obj, converting to array here.
-    const x = event.clientX
-    const y = event.clientY  
-    spanArray.forEach((el) => {
-        const ElementDimensions = el.getBoundingClientRect()
-        const elementX = (ElementDimensions.x + ElementDimensions.width / 2)
-        const elementY = (ElementDimensions.y + ElementDimensions.height / 2)
-        const d = distance(elementX, x, elementY, y)
-        const relativePos = (elementX - d) / elementX
-        el.animate([{
-            fontWeight: `clamp(1, ${1000 * Math.pow(relativePos, 2)}, 1000)`,
-            fontStretch: `${125 * Math.pow(relativePos, 2)}%`,
-            fontVariationSettings: `'ital' ${Math.pow(relativePos, 2)}`
-        }], {
-            fill: "forwards",
-            duration: 390,
-            easing: "ease-out"
-        })  
-    })
-}, 50)
+const title2 = document.querySelector('.title2')
 
-
-
-title.onmousemove = (event) => {
-    animateTextOnMove(event, title)
+function textAnimation(title, reverse = false) {
+    const characterList = []
+    const text = title.innerText
+    const textArray = text.split("")
+    title.innerHTML = ""
+    for (let i = 0; i < textArray.length; i++) {
+        const character = new Character(title, textArray[i], reverse) // add third argument for reverse or not reverse, adjust updateProperties accordingly.
+        characterList.push(character)
+    }
+    return characterList
 }
+const chars = textAnimation(title)
+const chars2 = textAnimation(title2, true)
 
-title.ontouchmove = event => animateTextOnMove(event.touches[0], title)
+
+function render(characterList) {
+    for (let i = 0; i < characterList.length; i++) {
+        characterList[i].updateProperties({
+            weight: characterList[i].weight,
+            width: characterList[i].width,
+            ital: characterList[i].ital
+        })
+    }
+}
+function animate() {
+    virtualPos.x += (mouse.x - virtualPos.x) / 20
+    virtualPos.y += (mouse.y - virtualPos.y) / 20
+    render(chars)
+    render(chars2)
+    requestAnimationFrame(animate)
+}
+animate()
